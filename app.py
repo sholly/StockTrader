1e979d21e92d246b897848326a376f8812b4c94b
Jay Shollenberger <jshollen@redhat.com>
2022-05-26 21:28:34 -0600

    gotta figure out how to post JSON now

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import json
 from decimal import * 
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from models.base import Base
 from models.trade import Trade, TradeType
 from sqlalchemy import create_engine
@@ -43,7 +43,6 @@ class DecimalEncoder(json.JSONEncoder):
 class TradeController(Resource):
     def get(self):
         dbSession = DBSession()
-        print(dbSession)
         Session = dbSession.createSession()
         session = Session()
         results = []
@@ -52,6 +51,35 @@ class TradeController(Resource):
         for trade in trades: 
             results.append(trade.as_dict())
         return results
+
+    def post(self):
+        dbSession = DBSession()
+        Session = dbSession.createSession()
+        session = Session()
+
+        parser = reqparse.RequestParser()
+        parser.add_argument('symbol')
+        parser.add_argument('price')
+        parser.add_argument('quantity')
+        parser.add_argument('tradetype')
+        args = parser.parse_args()
+        print(args)
+        symbol = args['symbol']
+        price = args['price']
+        quantity = args['quantity']
+        ttype = str(args['tradetype'])
+        tradetype
+        if ttype.lower() == "BUY":
+            tradetype = TradeType.buy
+        else:
+            tradetype = TradeType.sell
+
+        trade = Trade(symbol, price, quantity, tradetype, datetime.now())
+        session.add(trade)
+        session.commit()
+        session.close()
+
+
 
 api.add_resource(TradeController, '/trade')
 
